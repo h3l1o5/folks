e3aff1f06ffffa97751abcbb78e48fd65d204162
martysun25 <martysun25@gmail.com>
2017-08-20 21:06:55 +0800

    initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+
+import App from './App'
+import './index.scss'
+
+ReactDOM.render(<App />, document.getElementById('app'))
