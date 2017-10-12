ffc9500c549e888887baab09c127ba392307f1f7
martysun25 <martysun25@gmail.com>
2017-10-13 00:23:27 +0800

    Server: fix a route bug

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -6,14 +6,14 @@ const path = require('path')
 const apiRoute = require('../routes/api/v1')
 
 module.exports = app => {
-  app.use(express.static(path.join(__dirname, '/../build')))
+  app.use(express.static(path.join(__dirname, '/../../build')))
   app.use(bodyParser.json())
   app.use(bodyParser.urlencoded({ extended: false }))
   app.use(logger(process.env.NODE_ENV === 'production' ? 'common' : 'dev'))
 
   app.use('/api/v1/', apiRoute)
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/../build/index.html'))
+    res.sendFile(path.join(__dirname, '/../../build/index.html'))
   })
 
   app.use((err, req, res, next) => {
